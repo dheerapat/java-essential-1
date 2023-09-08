89dc07c6dc9e33d4f9a5be9c55c95f58171e249c
Dheerapat Tookkane <dheerapat38@gmail.com>
2023-09-08 22:56:00 +0700

    scanner input

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,9 +1,21 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
     public static void main(String[] args) {
-        int hours = 40;
-        double payRate = 25.50;
+        // variable initialization
+        int hours = 0;
+        System.out.println("How many hour did you work?");
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+
+        double payRate = 0;
+        System.out.println("What is your pay rate?");
+        payRate = scanner.nextDouble();
+
+        scanner.close();
+
         double grossPay = hours * payRate;
         System.out.println("Gross Pay: " + grossPay);
     }
