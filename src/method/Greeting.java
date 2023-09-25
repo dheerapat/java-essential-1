3598290fc3c511135d9654f1ace440a8ef06cad4
Dheerapat Tookkane <dheerapat38@gmail.com>
2023-09-25 21:40:33 +0700

    method

---
+++ src/method/Greeting.java
@@ -0,0 +1,15 @@
+package method;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greetUser();
+    }
+    public static void greetUser() {
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello " + name);
+    }
+}
